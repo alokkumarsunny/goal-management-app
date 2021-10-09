d82741a899999049049f2525d4f9a4b0777bdd78
Alok Kumar <alok.kumar@umail.ucc.ie>
2021-10-09 23:55:36 +0100

    App js

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Button, FlatList, StyleSheet, TextInput, View } from "react-native";
+import GoalInput from "./components/GoalInput";
+import GoalItem from "./components/goalItem";
 
 export default function App() {
+  const [listGoal, setListGoal] = useState([]);
+  const [isGoalInput, setIsGoalInput] = useState(false);
+
+  const renterList = (enterGoalValue) => {
+    setListGoal((oldList) => [
+      ...oldList,
+      { key: Math.random().toString(), value: enterGoalValue },
+    ]);
+    setIsGoalInput(false);
+  };
+
+  const onDeleteItemHandler = (enterGoalId) => {
+    setListGoal((currentList) =>
+      currentList.filter((goal1) => goal1.key !== enterGoalId)
+    );
+  };
+
+  const isGoalVisibleHandler = (value) => {
+    setIsGoalInput(value);
+  };
+
+  const onCancleHandler = () => {
+    setIsGoalInput(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Enter Goal" onPress={() => isGoalVisibleHandler(true)} />
+      <GoalInput
+        isVisible={isGoalInput}
+        onEnter={renterList}
+        onCancel={onCancleHandler}
+      />
+      <FlatList
+        data={listGoal}
+        renderItem={(renderData) => (
+          <GoalItem item={renderData.item} onDelete={onDeleteItemHandler} />
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30,
+    marginTop: 20,
   },
 });
